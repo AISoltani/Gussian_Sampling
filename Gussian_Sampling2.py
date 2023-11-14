9ebd820347d14c24c078599e5e0fcdc2fe71a1e0
AIsoltani <amirsoltani_os@hotmail.com>
2023-11-14 09:19:54 +0330

    Update Gussian_Sampling2.py

--- Gussian_Sampling2.py
+++ Gussian_Sampling2.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
+
 data_x = np.linspace(1.0, 10.0, 100)[:, np.newaxis]
 data_y = np.sin(data_x) + 0.1 * np.power(data_x, 2) + 0.5 * np.random.randn(100, 1)
 data_x /= np.max(data_x)
